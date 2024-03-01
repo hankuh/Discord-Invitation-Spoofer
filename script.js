5672412a34e4a9d95896d1108c4322a4272539bd
Hanku <unar2387@gmail.com>
2024-03-01 17:07:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 particlesJS('particles-js', {
     particles: {
-        number: { value: 80, density: { enable: true, value_area: 800 } },
+        number: { value: 160, density: { enable: true, value_area: 800 } },
         color: { value: "#66ccff" },
         shape: { type: "circle", stroke: { width: 0, color: "#000000" } },
         opacity: { value: 0.5, random: false },
